6f7ab4025f64a0f1d06b1eeef22e82964a131042
Srinath <sriinathk@gmail.com>
2019-05-17 21:34:45 +0530

    name srinath server path chnges (loaded from express)

--- server/index.js
+++ server/index.js
@@ -4,16 +4,17 @@ var apollo_server_1 = require("apollo-server");
 var opportunities_1 = require("./Resolvers/opportunities");
 var opportunities_2 = require("./Schema/opportunities");
 var express = require("express");
+var path = require("path");
 var app = express();
-var ApollographQLServer = new apollo_server_1.ApolloServer({
-    typeDefs: opportunities_2.typeDefs,
-    resolvers: opportunities_1.resolvers
-});
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '../dist/index.html');
+    res.sendFile(path.resolve('./index.html'));
 });
 app.listen({ port: process.env.PORT || 3000 }, function () {
     console.log("\uD83D\uDE80 Express Serverstarted");
+});
+var ApollographQLServer = new apollo_server_1.ApolloServer({
+    typeDefs: opportunities_2.typeDefs,
+    resolvers: opportunities_1.resolvers
 });
 ApollographQLServer.listen({ port: process.env.PORT || 4000 }).then(function (_a) {
     var url = _a.url;
