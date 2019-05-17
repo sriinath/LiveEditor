9e958c97a0657104ab3776164b7bd06ba0d5a8ce
Srinath <sriinathk@gmail.com>
2019-05-17 17:03:20 +0530

    name srinath

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,21 @@
 import { ApolloServer } from 'apollo-server'
 import { resolvers } from './Resolvers/opportunities'
 import { typeDefs } from './Schema/opportunities'
+import express from 'express'
+const app = express()
 
-const server = new ApolloServer({
+const ApollographQLServer = new ApolloServer({
     typeDefs,
     resolvers
 });
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '../dist/index.html')
+})
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+app.listen({ port: process.env.PORT || 3000 }, function() {
+    console.log(`🚀 Express Serverstarted`);
+})
+
+ApollographQLServer.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+    console.log(`🚀 GraphQL Server ready at ${url}`);
 });
